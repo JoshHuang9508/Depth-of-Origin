ce273b03fe5d8ce2959d113789172ec7015dddb1
youchentsai <youchentsai950324@gmail.com>
2023-10-19 00:03:05 +0800

    add item action

--- Assets/Script/UI/UIInventoryItem.cs
+++ Assets/Script/UI/UIInventoryItem.cs
@@ -79,42 +79,5 @@ namespace Inventory.UI
         {
             
         }
-
-
-        /*public void OnPointerClick(PointerEventData pointerData)
-        {
-            if (pointerData.button == PointerEventData.InputButton.Right)
-            {
-                OnRightMouseBtnClick?.Invoke(this);
-            }
-            else
-            {
-                OnItemClicked?.Invoke(this);
-            }
-        }
-
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            if (empty)
-            {
-                return;
-            }
-            OnItemBeginDrag?.Invoke(this);
-        }
-
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            OnItemEndDrag?.Invoke(this);
-        }
-
-        public void OnDrop(PointerEventData eventData)
-        {
-            OnItemDroppedOn?.Invoke(this);
-        }
-
-        public void OnDrag(PointerEventData eventData)
-        {
-
-        }*/
     }
 }
