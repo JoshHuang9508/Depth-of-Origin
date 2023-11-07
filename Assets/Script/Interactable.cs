a086db60e1b83e320586f34bfe4c06f300c588b1
youchentsai <youchentsai950324@gmail.com>
2023-11-07 23:29:35 +0800

    add interact Dialog

--- Assets/Script/Interactable.cs
+++ Assets/Script/Interactable.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 public class Interactable : MonoBehaviour
 {
+    public GameObject interactDialog;
+
+    public RectTransform temp;
 
     public bool isInRange;
 
@@ -15,7 +20,7 @@ public class Interactable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        interactDialog.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,6 +28,7 @@ public class Interactable : MonoBehaviour
     {
         if (isInRange)
         {
+            
             if (Input.GetKeyDown(interactKey))
             {
                 interactAction.Invoke();
@@ -34,6 +40,14 @@ public class Interactable : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            RectTransform text_Transform = Instantiate(interactDialog).GetComponent<RectTransform>();
+            text_Transform.gameObject.SetActive(true);
+            //text_Transform.transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position) + new Vector3(0, -200, 0);
+            text_Transform.transform.position = new Vector3(960, 200, 0);
+            text_Transform.SetParent(GameObject.FindFirstObjectByType<Canvas>().transform);
+            TMP_Text text = text_Transform.GetComponentInChildren<TMP_Text>();
+            text.text = $"Press {interactKey} to interact";
+            temp = text_Transform;
             isInRange = true;
             //Debug.Log("Player in range");
         }
@@ -43,6 +57,7 @@ public class Interactable : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            Destroy(temp.gameObject);
             isInRange = false;
             leaveRangeAction.Invoke();
             //Debug.Log("Player leave range");
