4345a01eecdc7b1bdb11127e2ba519c09530bc64
JoshHuang9508 <joshhuang9508@gmail.com>
2023-11-08 23:41:22 +0800

    - 新增地圖邊界，玩家跑出帶有"WorldEdge"Tag的Collider2D會使攝影機停止跟隨 - 修改和統一地圖設定

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Header("Setting")]
     public Transform target;
-
+    public Collider2D mapBounds;
     public float smoothing;
+    public Vector3 offset = Vector3.zero;
 
-    public Vector3 offset;
+    [Header("Status")]
+    public bool isFollowing_x = true;
+    public bool isFollowing_y = true;
 
     private void FixedUpdate()
     {
-        if (target != null)
+        try
         {
-            Vector3 newPosition = Vector3.Lerp(transform.position, target.transform.position + offset, smoothing);
+            mapBounds = GameObject.FindWithTag("WorldEdge").GetComponent<Collider2D>();
+        }
+        catch
+        {
+            mapBounds = null;
+        }
+
+        Vector3 newPosition = Vector3.Lerp(transform.position, target.transform.position + offset, smoothing);
+
+        if(mapBounds != null)
+        {
+            Vector2 targetPos_x = new Vector2(newPosition.x, 0);
+            Vector2 targetPos_y = new Vector2(0, newPosition.y);
+
+            if (!mapBounds.bounds.Contains(targetPos_x)) isFollowing_x = false;
+            else isFollowing_x = true;
+
+            if (!mapBounds.bounds.Contains(targetPos_y)) isFollowing_y = false;
+            else isFollowing_y = true;
+
+            Vector3 currentPos = transform.position;
+
+            transform.position = new Vector3(isFollowing_x ? newPosition.x : currentPos.x, isFollowing_y ? newPosition.y : currentPos.y, newPosition.z);
+        }
+        else
+        {
             transform.position = newPosition;
         }
     }
